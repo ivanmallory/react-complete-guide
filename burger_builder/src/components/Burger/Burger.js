33c17802495d2765fb096194c3789b50f3f7e15c
Ivan Mallory <ivanmallory1992@gmail.com>
2021-01-01 17:27:54 -0600

    Added dynamic rendering of burger ingredients by initializing state

--- burger_builder/src/components/Burger/Burger.js
+++ burger_builder/src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //Have to convert object of key value pairs to an array
+    const transformedIngredients = Object.keys(props.ingredients) 
+        .map(igKey => { //Maps out objects in a list
+            return [...Array(props.ingredients[igKey])].map((_, i) => { //Maps out objects in an array
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );  
