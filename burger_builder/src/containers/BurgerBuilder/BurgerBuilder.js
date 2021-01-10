6ee0a5d7614f31ac5c3909184692f3eefdd7538e
Ivan Mallory <ivanmallory1992@gmail.com>
2021-01-10 16:32:34 -0600

    Added Navigation functionality and styling. Working on toggling and animating SideDrawer

--- burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,6 +70,9 @@ class BurgerBuilder extends Component {
     purchaseCancelHandler = () => {
         this.setState({purchasing: false})
     }
+    purchaseContinueHandler = () => {
+        alert('You continue!')
+    }
     render(){
         const disabledInfo = {
             ...this.state.ingredients
@@ -81,7 +84,12 @@ class BurgerBuilder extends Component {
         return(
             <Auxiliary>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        price={this.state.totalPrice}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
